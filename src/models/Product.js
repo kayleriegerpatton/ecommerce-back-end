c5330a44ff8d0274ef70fbea178a980bfc7724fd
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-12-13 13:56:25 +0000

    product model

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,23 +1,30 @@
-// import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
-const sequelize = require('../config/connection');
+// external imports
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
-Product.init(
-  {
-    // define columns
+const attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
-);
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "product",
+};
+
+Product.init(attributes, options);
 
 module.exports = Product;
