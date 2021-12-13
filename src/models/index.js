fa19c386f2fc67c4db3ae20a34fd7904395eaca7
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-12-13 15:05:14 +0000

    table associations

--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,45 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// ASSOCIATIONS 
+// ASSOCIATIONS
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  // when a product is deleted, set its id to null in category table
+  onDelete: "SET NULL",
+  onUpdate: "CASCADE",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "product_id",
+  // when category is deleted, set its id as null in product table
+  onDelete: "SET NULL",
+  onUpdate: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  // onDelete: "",
+  // onUpdate: "",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  // onDelete: "",
+  // onUpdate: "",
+});
 
 module.exports = {
   Product,
