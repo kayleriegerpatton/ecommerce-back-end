675ef164b47ee0983e1834eb403efda98dac1c9d
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-12-13 14:13:26 +0000

    product tag model

--- src/models/ProductTag.js
+++ src/models/ProductTag.js
@@ -1,20 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
+const attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "product",
+      key: "id",
+    },
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "tag",
+      key: "id",
+    },
+  },
+};
+
+const options = {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "product_tag",
+};
+
+ProductTag.init(attributes, options);
 
 module.exports = ProductTag;
